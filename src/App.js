c56b8566c6a40d60fd5bd5015b11f90e7c341b95
Martin Crabtree <martin.crabtree@gmail.com>
2023-02-20 22:52:34 -0500

    fleshed out basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Col } from "react-bootstrap";
+// import "./App.scss";
+import { Container, Row, Col, Carousel } from "react-bootstrap";
 import { Cart } from "./components/Cart";
 import { Card } from "./components/Card";
+import styled from "styled-components";
 
 import ships from "./data.json";
 
+const BackgroundWrapper = styled.div`
+  background-color: black;
+`;
+
+const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 function App() {
+  const [index, setIndex] = useState(0);
+
   const _renderCards = (dataArr) => {
     return dataArr.map((data) => {
-      return <Card {...data} />;
+      return (
+        <Carousel.Item interval={200000}>
+          <CardWrapper>
+            <Card {...data} />
+          </CardWrapper>
+        </Carousel.Item>
+      );
     });
   };
 
+  const _handleSelect = (cardIndex, event) => {
+    setIndex(cardIndex);
+  };
+
   return (
-    <Container>
-      <header>
-        <h1>Eve Online Shop Demo</h1>
-      </header>
-      <Row>{ships && _renderCards(ships)}</Row>
-    </Container>
+    <BackgroundWrapper>
+      <Container>
+        <header>
+          <h1>Eve Online Shop Demo</h1>
+        </header>
+        <Row>
+          <Carousel activeIndex={index} onSelect={_handleSelect}>
+            {ships && _renderCards(ships)}
+          </Carousel>
+        </Row>
+      </Container>
+    </BackgroundWrapper>
   );
 }
 
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,26 @@
 import React from "react";
-import { Toast } from "react-bootstrap";
+import { Toast, ToastContainer } from "react-bootstrap";
+import styled from "styled-components";
+
+const CardContainer = styled.div`
+  min-height: 800px;
+`;
 
 const Card = (props) => {
   const { id, name, image, info, price } = props;
 
   return (
     <Toast id={id}>
-      <Toast.Header>
-        <strong className="temp">Ship: {name}</strong>
-      </Toast.Header>
-      <Toast.Body>Info: {info}</Toast.Body>
-      <Toast.Body>Price: {price.toString()} ISK</Toast.Body>
+      <CardContainer>
+        <Toast.Header>
+          <strong className="temp">Ship: {name}</strong>
+        </Toast.Header>
+        <Toast.Body>
+          <img src={image} alt={name} />
+        </Toast.Body>
+        <Toast.Body>Info: {info}</Toast.Body>
+        <Toast.Body>Price: {price.toString()} ISK</Toast.Body>
+      </CardContainer>
     </Toast>
   );
 };
